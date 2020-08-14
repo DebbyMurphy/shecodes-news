22f03e56a640097abba5e0c59211b98898f4250e
DebbyMurphy <debby.m.jones@gmail.com>
2020-08-14 16:53:24 +0800

    CSS OK across all pages

--- she_codes_news/news/models.py
+++ she_codes_news/news/models.py
@@ -6,7 +6,7 @@ class NewsStory(models.Model):
     title = models.CharField(max_length=200)
     pub_date = models.DateTimeField()
     content = models.TextField()
-    image_url = models.URLField(max_length=200)
+    image_url = models.URLField(max_length=200, default="https://images.unsplash.com/photo-1552912140-6b3c254f5214?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80")
     author = models.ForeignKey(
         get_user_model(),
         on_delete=models.CASCADE, 
